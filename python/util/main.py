e0cf9af7d1227383cec6534e39dd4866a3610d76
Your Name <you@example.com>
2014-04-08 12:10:33 -0400

    update

--- python/util/main.py
+++ python/util/main.py
@@ -1,41 +1,50 @@
-""" An example of how to insert a document """
-#import sys
-#from datetime import datetime
-#from pymongo import Connection
-#from pymongo.errors import ConnectionFailure
 import csv
-from lib2to3.patcomp import _type_of_literal
+import re
 
 
-def main():
+def cleanCells(fname):
 
     #Store the imported csv data as a list to output
-    data = []
+    csvOut = []
 
     # open the file
-    fname = '/home/jvwong/Documents/GCIF/data/2014_gcif.csv'
-    # fname = '/home/jvwong/Projects/GCIF/data/2014_gcif.csv'
     with open(fname, 'rb') as csvfile:
 
         #Instantiate a csv reader
         csvreader = csv.reader(csvfile, delimiter=',')
 
         #Read in first row of headers
-        column = csvreader.next()
-        # print column[1:5]
+        header = csvreader.next()
+        csvOut.append(header)
 
         #loop through rows
-        for idx, row in enumerate(csvreader):
-            data.append(row)
-            print idx
+        for idxr, row in enumerate(csvreader):
+
+            #loop through cells
+            for idxc, cell in enumerate(row):
+
+                # Ignore any comment sections, URLS
+                if not re.match('comment', cell.lower()) and not re.match('^http://', cell.lower()):
+                    strip1 = re.sub('[\$,%]', "", cell) #strip units
+                    strip2 = re.sub('N/A', "", strip1.upper()) #remove N/A fields
+                    row[idxc] = strip2
+
+            csvOut.append(row)
+
+    return csvOut
 
 
-    print (data[0][0])
-    print len(data[0])
 
-    # with open('test.csv', 'wb') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerows(someiterable)
+
+def main():
+
+    fin = '/home/jvwong/Documents/GCIF/data/2008_gcif.csv'
+    fout = fin.split('/')[-1]
+    csvOut = cleanCells(fin)
+
+    with open(fout, 'wb') as fout:
+        writer = csv.writer(fout)
+        writer.writerows(csvOut)
 
 
 if __name__ == "__main__":
