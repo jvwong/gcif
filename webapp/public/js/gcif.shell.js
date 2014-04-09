06a45cce684a454df02bbc3c791fb0eb3ed940cb
Your Name <you@example.com>
2014-04-09 17:26:45 -0400

    bootstrap messing

--- webapp/public/js/gcif.shell.js
+++ webapp/public/js/gcif.shell.js
@@ -20,11 +20,32 @@ gcif.shell = (function () {
 
         main_html : String() +
 
+             '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">' +
+                  '<div class="container">' +
+                    '<div class="navbar-header">' +
+                      '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' +
+                        '<span class="sr-only">Toggle navigation</span>' +
+                        '<span class="icon-bar"></span>' +
+                        '<span class="icon-bar"></span>' +
+                        '<span class="icon-bar"></span>' +
+                      '</button>' +
+                      '<a class="navbar-brand" href="#">Project name</a>' +
+                    '</div>' +
+                    '<div class="collapse navbar-collapse">' +
+                      '<ul class="nav navbar-nav">' +
+                        '<li class="active"><a href="#">Home</a></li>' +
+                        '<li><a href="#about">About</a></li>' +
+                        '<li><a href="#contact">Contact</a></li>' +
+                      '</ul>' +
+                    '</div><!--/.nav-collapse -->' +
+                  '</div>' +
+                '</div>' +
+
             '<div class="content">' +
                 '<div class="container">' +
-                    '<p>summy content</p>'+
                 '</div>' +
             '</div>'
+
 
     }
     , stateMap = {
@@ -83,7 +104,7 @@ gcif.shell = (function () {
         setJqueryMap();
 
         // configure and initialize feature modules
-        gcif.dash.initModule( jqueryMap.$container );
+//        gcif.dash.initModule( jqueryMap.$container );
 
     };
     // End PUBLIC method /initModule/
