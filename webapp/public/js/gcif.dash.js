50c404d9b4403d0579f113d8874a8bed30b00ed7
Your Name <you@example.com>
2014-04-10 10:51:47 -0400

    added pastel swatch

--- webapp/public/js/gcif.dash.js
+++ webapp/public/js/gcif.dash.js
@@ -21,61 +21,61 @@ gcif.dash = (function () {
         main_html : String() +
 
             '<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">' +
-                '<h1 class="page-header">Dashboard</h1>' +
+                '<h3 class="page-header">Category Section</h3>' +
 
                 '<div class="row placeholders">' +
                     '<div class="col-xs-6 col-sm-3 placeholder">' +
-                        '<img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">' +
-                        '<h4>Label</h4>' +
-                        '<span class="text-muted">Something else</span>' +
+                        '<img src="assets/blue-pastel.png" class="img-responsive img-circle" alt="Placeholder thumbnail" />' +
+                        '<h4>Explore</h4>' +
+                        '<span class="text-muted">Data Visualization</span>' +
                     '</div>' +
-                '<div class="col-xs-6 col-sm-3 placeholder">' +
-                    '<img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">' +
-                    '<h4>Label</h4>' +
-                    '<span class="text-muted">Something else</span>' +
-                '</div>' +
-                '<div class="col-xs-6 col-sm-3 placeholder">' +
-                    '<img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">' +
-                    '<h4>Label</h4>' +
-                    '<span class="text-muted">Something else</span>' +
-                '</div>' +
-                '<div class="col-xs-6 col-sm-3 placeholder">' +
-                    '<img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">' +
-                    '<h4>Label</h4>' +
-                    '<span class="text-muted">Something else</span>' +
-                '</div>' +
-            '</div>' +
+                    '<div class="col-xs-6 col-sm-3 placeholder">' +
+                        '<img src="assets/brown-pastel.png" class="img-responsive img-circle" alt="Placeholder thumbnail" />' +
+                        '<h4>Learn</h4>' +
+                        '<span class="text-muted">Materials</span>' +
+                    '</div>' +
+                    '<div class="col-xs-6 col-sm-3 placeholder">' +
+                        '<img src="assets/mint-pastel.png" class="img-responsive img-circle" alt="Placeholder thumbnail" />' +
+                        '<h4>Compare</h4>' +
+                        '<span class="text-muted">Data Analysis</span>' +
+                    '</div>' +
+                    '<div class="col-xs-6 col-sm-3 placeholder">' +
+                        '<img src="assets/violet-pastel.png" class="img-responsive img-circle" alt="Placeholder thumbnail" />' +
+                        '<h4>Share</h4>' +
+                        '<span class="text-muted">Connect</span>' +
+                    '</div>' +
+                 '</div>' +
 
-            '<h2 class="sub-header">Section title</h2>' +
-            '<div class="table-responsive">' +
-                '<table class="table table-striped">' +
-                    '<thead>' +
-                        '<tr>' +
-                            '<th>#</th>' +
-                            '<th>Header</th>' +
-                            '<th>Header</th>' +
-                            '<th>Header</th>' +
-                            '<th>Header</th>' +
-                        '</tr>' +
-                    '</thead>' +
-                    '<tbody>' +
-                        '<tr>' +
-                            '<td>1,001</td>' +
-                            '<td>Lorem</td>' +
-                            '<td>ipsum</td>' +
-                            '<td>dolor</td>' +
-                            '<td>sit</td>' +
-                        '</tr>' +
-                        '<tr>' +
-                            '<td>1,002</td>' +
-                            '<td>amet</td>' +
-                            '<td>consectetur</td>' +
-                            '<td>adipiscing</td>' +
-                            '<td>elit</td>' +
-                        '</tr>' +
-                    '</tbody>' +
-                '</table>' +
-            '</div>'
+                '<h3 class="sub-header">Visualization Section</h3>' +
+                '<div class="table-responsive">' +
+                    '<table class="table table-striped">' +
+                        '<thead>' +
+                            '<tr>' +
+                                '<th>#</th>' +
+                                '<th>Header</th>' +
+                                '<th>Header</th>' +
+                                '<th>Header</th>' +
+                                '<th>Header</th>' +
+                            '</tr>' +
+                        '</thead>' +
+                        '<tbody>' +
+                            '<tr>' +
+                                '<td>1,001</td>' +
+                                '<td>Lorem</td>' +
+                                '<td>ipsum</td>' +
+                                '<td>dolor</td>' +
+                                '<td>sit</td>' +
+                            '</tr>' +
+                            '<tr>' +
+                                '<td>1,002</td>' +
+                                '<td>amet</td>' +
+                                '<td>consectetur</td>' +
+                                '<td>adipiscing</td>' +
+                                '<td>elit</td>' +
+                            '</tr>' +
+                        '</tbody>' +
+                    '</table>' +
+                '</div>'
     }
     , stateMap = {
         $container : undefined
