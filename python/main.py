6a66115876856e3db1a2b11efc6a52314965cead
Your Name <you@example.com>
2014-04-11 09:58:32 -0400

    removed node modules

--- python/main.py
+++ python/main.py
@@ -4,13 +4,13 @@ from gcif_util import *
 def main():
 
     ### *********** Per city indicators (remove comment, n/a, data year)
-    fin = '/home/jvwong/Projects/GCIF/data/clean_compile.csv'
-    fout = "noText_" + fin.split('/')[-1]
-    csvout = perCityFilter(fin)
-
-    with open(fout, 'wb') as fout:
-        writer = csv.writer(fout)
-        writer.writerows(csvout)
+    # fin = '/home/jvwong/Projects/GCIF/data/clean_compile.csv'
+    # fout = "noText_" + fin.split('/')[-1]
+    # csvout = perCityFilter(fin)
+    #
+    # with open(fout, 'wb') as fout:
+    #     writer = csv.writer(fout)
+    #     writer.writerows(csvout)
 
 
     ### *********** Automated d3 data type processing
@@ -19,16 +19,15 @@ def main():
     # with open('/home/jvwong/Projects/GCIF/webapp/bdd/gcif_dash/src/parseData.js', 'wb') as fout:
     #     fout.write(jsText)
 
-    ### ***********Initial data cleaning
-    # fin = '/home/jvwong/Documents/GCIF/data/2014_gcif.csv'
-    # fout = fin.split('/')[-1]
-    # mismatch = checkHeaders('/home/jvwong/Documents/GCIF/data/2008_gcif.csv', '/home/jvwong/Documents/GCIF/data/2014_gcif.csv')
-    # csvOut = cleanCells(fin)
-    # with open(fout, 'wb') as fout:
-    #     writer = csv.writer(fout)
-    #     writer.writerows(csvOut)
-    # fin = '/home/jvwong/Documents/GCIF/data/2014_gcif.csv'
-    # fout = fin.split('/')[-1]
+    ### *********** Cleaning of csv data removing units, commas, "n/a" fields
+    fin = '/home/jvwong/Documents/GCIF/data/recent_gcif.csv'
+    fout = fin.split('/')[-1]
+
+    csvOut = cleanCells(fin)
+
+    with open(fout, 'wb') as fout:
+        writer = csv.writer(fout)
+        writer.writerows(csvOut)
 
 
 
